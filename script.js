3931f9aac5072eea11b7dd87a763c6108e235221
micka <mickalarissa01@gmail.com>
2023-01-22 23:33:41 -0300

    aparecendoasmensgensconformelayout

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const nome=prompt("Qual seu lindo nome?");
 
-let erro=0;
+let Mal=0;
 
 const nomes={name:nome};
 
@@ -20,26 +20,75 @@ setInterval(online, 5000)
 
 function buscarnoserv(){
     const busca=axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    busca.then(respostachegou)
 }
+
+function respostachegou(respostaa){
+    console.log(respostaa);
+    renderizamsg(respostaa.data);
+}
+
+function renderizamsg(mensage){
+
+
+    const lista=document.querySelector('.chat');
+    
+
+
+    lista.innerHTML='';
+
+    for(let index = 0; index < mensage.length; index++){
+        const tipo=(mensage[index].type)
+        if(tipo==="status"){
+       lista.innerHTML= lista.innerHTML +  `<div class="entrouousaiu msg">
+         <span class="hora">(${mensage[index].time}) </span>
+         <span class="name"> ${mensage[index].from}</span>
+         <span class="msgm">  ${mensage[index].text} </span>
+         </div>
+    `}else if(tipo==="message"){
+        lista.innerHTML= lista.innerHTML +  `<div class="normal msg">
+         <span class="hora">(${mensage[index].time}) </span>
+         <span class="name"> ${mensage[index].from}</span>
+         <span class="to">para ${mensage[index].to}:</span>
+         <span class="msgm">  ${mensage[index].text} </span>
+         </div>`
+    }else{
+        const destino=(mensage[index].to)
+        if(destino===nome){
+        lista.innerHTML= lista.innerHTML +  `<div class="reservadamente msg">
+        <span class="hora">(${mensage[index].time}) </span>
+        <span class="name"> ${mensage[index].from}</span>
+        <span class="to">para ${mensage[index].to}:</span>
+        <span class="msgm">  ${mensage[index].text} </span>
+        </div>`}
+    }
+}
+
+}
+
+
 
 function promessarespossta(resposta){
 console.log(resposta.data);
 console.log("podeentrraar");
-}
 
+}
+setInterval(buscarnoserv,300)
 
 function tratarErro(erro) {
      
       console.log("Status code: " + erro.response.status); // Ex: 404
       console.log("Mensagem de erro: " + erro.response.data); // Ex: Not Found
       console.log('Deu erro ao botar os nomes no servidor :(');
-   erro=1;
+   Mal=1;
    deuruim()  
 }
 
 function deuruim(){
-    while(erro==1){
-        nome=prompt("Deu ruim, insira outro nome por favor:")
+  while(Mal !== 0){
+        alert("Deu ruim, insira outro nome por favor:");
+        window.location.reload()
+        Mal=0;
     }
 }
 
